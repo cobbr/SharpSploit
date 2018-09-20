77a1b5a350bad0d324d093df01f8cbeae24fedb5
cobbr <ryancobb65@gmail.com>
2018-09-20 09:23:16 -0500

    SharpSploit v1.0

---
+++ SharpSploit/Misc/Utilities.cs
@@ -0,0 +1,48 @@
+﻿// Author: Ryan Cobb (@cobbr_io)
+// Project: SharpSploit (https://github.com/cobbr/SharpSploit)
+// License: BSD 3-Clause
+
+using System.IO;
+using System.IO.Compression;
+
+namespace SharpSploit.Misc
+{
+    public class Utilities
+    {
+        public static byte[] Compress(byte[] Bytes)
+        {
+            byte[] compressedBytes;
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (DeflateStream deflateStream = new DeflateStream(memoryStream, CompressionMode.Compress))
+                {
+                    deflateStream.Write(Bytes, 0, Bytes.Length);
+                }
+                compressedBytes = memoryStream.ToArray();
+            }
+            return compressedBytes;
+        }
+
+        public static byte[] Decompress(byte[] compressed)
+        {
+            using (MemoryStream inputStream = new MemoryStream(compressed.Length))
+            {
+                inputStream.Write(compressed, 0, compressed.Length);
+                inputStream.Seek(0, SeekOrigin.Begin);
+                using (MemoryStream outputStream = new MemoryStream())
+                {
+                    using (DeflateStream deflateStream = new DeflateStream(inputStream, CompressionMode.Decompress))
+                    {
+                        byte[] buffer = new byte[4096];
+                        int bytesRead;
+                        while ((bytesRead = deflateStream.Read(buffer, 0, buffer.Length)) != 0)
+                        {
+                            outputStream.Write(buffer, 0, bytesRead);
+                        }
+                    }
+                    return outputStream.ToArray();
+                }
+            }
+        }
+    }
+}
