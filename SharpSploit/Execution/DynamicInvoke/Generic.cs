8a310b2f8e522f356ea96ba7ff890b6ee5adc1dc
TheWover <17090738+TheWover@users.noreply.github.com>
2019-07-26 13:59:28 -0700

    Added library for dynamically loading and executing API calls to avoid PInvoke. Also added several examples to begin a regularly updated set of dynamic delegates.

---
+++ SharpSploit/Execution/DynamicInvoke/Generic.cs
@@ -0,0 +1,68 @@
+﻿// Author: Ryan Cobb (@cobbr_io), The Wover (@TheRealWover)
+// Project: SharpSploit (https://github.com/cobbr/SharpSploit)
+// License: BSD 3-Clause
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace SharpSploit.Execution.DynamicInvoke
+{
+    /// <summary>
+    /// Contains function prototypes for third-party code, and functionality for dynamically invoking arbitrary API calls from memory or disk.
+    /// Allows you to avoid suspicious P/Invokes, imports, and IAT entries by loading modules and invoking their functions at runtime rather than referencing them at compile-time.
+    /// </summary>
+    class Generic
+    {
+
+        /// <summary>
+        /// Dynamically invoke an arbitrary function from a DLL, providing its name, function prototype, and arguments.
+        /// </summary>
+        /// <param name="dllName">Name of the DLL.</param>
+        /// <param name="procedureName">Name of the function.</param>
+        /// <param name="funcType">Prototype for the function, represented as a Delegate object.</param>
+        /// <param name="args">Arbitrary set of arguments to pass it. Can be modified if function uses call by reference.</param>
+        /// <returns>Anything returned by the function. Must be unmarshalled by the caller.</returns>
+        public static object DynamicAPIInvoke(string dllName, string procedureName, Type funcType, ref object[] args)
+        {
+            IntPtr hModule = Execution.Win32.Kernel32.LoadLibrary(dllName);
+
+            IntPtr pFunction = Execution.Win32.Kernel32.GetProcAddress(hModule, procedureName);
+
+            Delegate funcDelegate = Marshal.GetDelegateForFunctionPointer(pFunction, funcType);
+
+            Object result = funcDelegate.DynamicInvoke(args);
+
+            return result;
+        }
+
+
+        /// <summary>
+        /// Dynamically invokes an arbitrary function from a pointer. Useful for manually mapped modules or loading/invoking unmanaged code from memory.
+        /// </summary>
+        /// <param name="pFunction">A pointer to the unmanaged function.</param>
+        /// <param name="funcType">Prototype for the function, represented as a Delegate object.</param>
+        /// <param name="args">Arbitrary set of arguments to pass it. Can be modified if function uses call by reference.</param>
+        /// <returns>Anything returned by the function. Must be unmarshalled by the caller.</returns>
+        public static object DynamicFunctionInvoke(IntPtr pFunction, Type funcType, ref object[] args)
+        {
+            Delegate funcDelegate = Marshal.GetDelegateForFunctionPointer(pFunction, funcType);
+
+            Object result = funcDelegate.DynamicInvoke(args);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Helper for getting the pointer to a function from a DLL on disk.
+        /// </summary>
+        /// <param name="dllName">The path to the DLL on disk. Uses the LoadLibrary convention.</param>
+        /// <param name="procedureName">Name of the exported procedure.</param>
+        /// <returns>Handle to the function.</returns>
+        public static IntPtr GetLibraryAddress(string dllName, string procedureName)
+        {
+            IntPtr hModule = Execution.Win32.Kernel32.LoadLibrary(dllName);
+
+            return Execution.Win32.Kernel32.GetProcAddress(hModule, procedureName);
+        }
+    }
+}
---
+++ SharpSploit/Execution/DynamicInvoke/Win32.cs
@@ -0,0 +1,37 @@
+﻿// Author: Ryan Cobb (@cobbr_io), The Wover (@TheRealWover)
+// Project: SharpSploit (https://github.com/cobbr/SharpSploit)
+// License: BSD 3-Clause
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace SharpSploit.Execution.DynamicInvoke
+{
+    /// <summary>
+    /// Contains function prototypes and wrapper functions for dynamically invoking Win32 API Calls.
+    /// </summary>
+    public class Win32
+    {
+        public static IntPtr OpenProcess(Execution.Win32.Kernel32.ProcessAccessFlags dwDesiredAccess, bool bInheritHandle, UInt32 dwProcessId)
+        {
+            //Craft an array for the arguments
+            object[] funcargs =
+            {
+                dwDesiredAccess, bInheritHandle, dwProcessId
+            };
+
+            return (IntPtr)Generic.DynamicAPIInvoke(@"kernel32.dll", @"OpenProcess",
+                typeof(DELEGATES.OpenProcess), ref funcargs);
+        }
+
+        public struct DELEGATES
+        {
+            [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+            public delegate IntPtr OpenProcess(
+                Execution.Win32.Kernel32.ProcessAccessFlags dwDesiredAccess,
+                bool bInheritHandle,
+                UInt32 dwProcessId
+            );
+        }
+    }
+}
