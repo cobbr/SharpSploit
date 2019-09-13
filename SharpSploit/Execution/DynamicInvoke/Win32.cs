1651f6973564aa93e5eb33c12cf05318bcf3a05c
TheWover <>
2019-09-13 16:16:28 -0500

    Added wrapper for IsWow64Process

--- SharpSploit/Execution/DynamicInvoke/Win32.cs
+++ SharpSploit/Execution/DynamicInvoke/Win32.cs
@@ -32,6 +32,28 @@ namespace SharpSploit.Execution.DynamicInvoke
                 typeof(Delegates.OpenProcess), ref funcargs);
         }
 
+        /// <summary>
+        /// Uses DynamicInvocation to call the IsWow64Process Win32 API. https://docs.microsoft.com/en-us/windows/win32/api/wow64apiset/nf-wow64apiset-iswow64process
+        /// </summary>
+        /// <returns>Returns true if process is WOW64, and false if not (64-bit, or 32-bit on a 32-bit machine).</returns>
+        public static bool IsWow64Process(System.IntPtr hProcess, ref bool lpSystemInfo)
+        {
+
+            //Build the set of parameters to pass in to IsWow64Process
+            object[] funcargs =
+            {
+                hProcess, lpSystemInfo
+            };
+
+            bool retVal = (bool)SharpSploit.Execution.DynamicInvoke.Generic.DynamicAPIInvoke(
+                @"kernel32.dll", @"IsWow64Process", typeof(Delegates.IsWow64Process), ref funcargs);
+
+            lpSystemInfo = (bool) funcargs[1];
+
+            //Dynamically load and invoke the API call with out parameters
+            return retVal;
+        }
+
         private static class Delegates
         {
             [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
@@ -39,6 +61,11 @@ namespace SharpSploit.Execution.DynamicInvoke
                 Execution.Win32.Kernel32.ProcessAccessFlags dwDesiredAccess,
                 bool bInheritHandle,
                 UInt32 dwProcessId
+            );
+
+            [UnmanagedFunctionPointer(CallingConvention.StdCall)]
+            public delegate bool IsWow64Process(
+                System.IntPtr hProcess, ref bool lpSystemInfo
             );
         }
     }
