45f12bf78b54f974c13d39a668546281835224ae
b33f <ru.boonen@gmail.com>
2019-11-10 20:32:20 +0000

    Function by hash

--- SharpSploit/Execution/DynamicInvoke/Generic.cs
+++ SharpSploit/Execution/DynamicInvoke/Generic.cs
@@ -8,6 +8,8 @@ using System.Diagnostics;
 using System.Runtime.InteropServices;
 
 using Execute = SharpSploit.Execution;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SharpSploit.Execution.DynamicInvoke
 {
@@ -122,6 +124,34 @@ namespace SharpSploit.Execution.DynamicInvoke
         }
 
         /// <summary>
+        /// Helper for getting the pointer to a function from a DLL loaded by the process.
+        /// </summary>
+        /// <author>Ruben Boonen (@FuzzySec)</author>
+        /// <param name="DLLName">The name of the DLL (e.g. "ntdll.dll" or "C:\Windows\System32\ntdll.dll").</param>
+        /// <param name="FunctionHash">Hash of the exported procedure.</param>
+        /// <param name="Key">64-bit integer to initialize the keyed hash object (e.g. 0xabc or 0x1122334455667788).</param>
+        /// <param name="CanLoadFromDisk">Optional, indicates if the function can try to load the DLL from disk if it is not found in the loaded module list.</param>
+        /// <returns>IntPtr for the desired function.</returns>
+        public static IntPtr GetLibraryAddress(string DLLName, string FunctionHash, Int64 Key, bool CanLoadFromDisk = false)
+        {
+            IntPtr hModule = GetLoadedModuleAddress(DLLName);
+            if (hModule == IntPtr.Zero && CanLoadFromDisk)
+            {
+                hModule = LoadModuleFromDisk(DLLName);
+                if (hModule == IntPtr.Zero)
+                {
+                    throw new FileNotFoundException(DLLName + ", unable to find the specified file.");
+                }
+            }
+            else if (hModule == IntPtr.Zero)
+            {
+                throw new DllNotFoundException(DLLName + ", Dll was not found.");
+            }
+
+            return GetExportAddress(hModule, FunctionHash, Key);
+        }
+
+        /// <summary>
         /// Helper for getting the base address of a module loaded by the current process. This base address could be passed to GetProcAddress/LdrGetProcedureAddress or it could be used for manual export parsing.
         /// </summary>
         /// <author>Ruben Boonen (@FuzzySec)</author>
@@ -139,6 +169,23 @@ namespace SharpSploit.Execution.DynamicInvoke
             }
 
             return IntPtr.Zero;
+        }
+
+        /// <summary>
+        /// Generate an HMAC-MD5 hash of the supplied string using an Int64 as the key. This is useful for unique hash based API lookups.
+        /// </summary>
+        /// <author>Ruben Boonen (@FuzzySec)</author>
+        /// <param name="APIName">API name to hash.</param>
+        /// <param name="Key">64-bit integer to initialize the keyed hash object (e.g. 0xabc or 0x1122334455667788).</param>
+        /// <returns>String, the computed MD5 hash value.</returns>
+        public static String GenerateUniqueAPIHash(String APIName, Int64 Key)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(APIName.ToLower());
+            byte[] kbytes = BitConverter.GetBytes(Key);
+
+            HMACMD5 hmac = new HMACMD5(kbytes);
+            byte[] bHash = hmac.ComputeHash(data);
+            return BitConverter.ToString(bHash).Replace("-", "");
         }
 
         /// <summary>
@@ -200,6 +247,70 @@ namespace SharpSploit.Execution.DynamicInvoke
             {
                 // Export not found
                 throw new MissingMethodException(ExportName + ", export not found.");
+            }
+            return FunctionPtr;
+        }
+
+        /// <summary>
+        /// Given a module base address, resolve the address of a function by manually walking the module export table.
+        /// </summary>
+        /// <author>Ruben Boonen (@FuzzySec)</author>
+        /// <param name="ModuleBase">A pointer to the base address where the module is loaded in the current process.</param>
+        /// <param name="FunctionHash">Hash of the exported procedure.</param>
+        /// <param name="Key">64-bit integer to initialize the keyed hash object (e.g. 0xabc or 0x1122334455667788).</param>
+        /// <returns>IntPtr for the desired function.</returns>
+        public static IntPtr GetExportAddress(IntPtr ModuleBase, string FunctionHash, Int64 Key)
+        {
+            IntPtr FunctionPtr = IntPtr.Zero;
+            try
+            {
+                // Traverse the PE header in memory
+                Int32 PeHeader = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + 0x3C));
+                Int16 OptHeaderSize = Marshal.ReadInt16((IntPtr)(ModuleBase.ToInt64() + PeHeader + 0x14));
+                Int64 OptHeader = ModuleBase.ToInt64() + PeHeader + 0x18;
+                Int16 Magic = Marshal.ReadInt16((IntPtr)OptHeader);
+                Int64 pExport = 0;
+                if (Magic == 0x010b)
+                {
+                    pExport = OptHeader + 0x60;
+                }
+                else
+                {
+                    pExport = OptHeader + 0x70;
+                }
+
+                // Read -> IMAGE_EXPORT_DIRECTORY
+                Int32 ExportRVA = Marshal.ReadInt32((IntPtr)pExport);
+                Int32 OrdinalBase = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x10));
+                Int32 NumberOfFunctions = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x14));
+                Int32 NumberOfNames = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x18));
+                Int32 FunctionsRVA = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x1C));
+                Int32 NamesRVA = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x20));
+                Int32 OrdinalsRVA = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + ExportRVA + 0x24));
+
+                // Loop the array of export name RVA's
+                for (int i = 0; i < NumberOfNames; i++)
+                {
+                    String FunctionName = Marshal.PtrToStringAnsi((IntPtr)(ModuleBase.ToInt64() + Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + NamesRVA + i * 4))));
+                    if (GenerateUniqueAPIHash(FunctionName.ToLower(), Key).ToLower() == FunctionHash.ToLower())
+                    {
+                        Int32 FunctionOrdinal = Marshal.ReadInt16((IntPtr)(ModuleBase.ToInt64() + OrdinalsRVA + i * 2)) + OrdinalBase;
+                        Int32 FunctionRVA = Marshal.ReadInt32((IntPtr)(ModuleBase.ToInt64() + FunctionsRVA + (4 * (FunctionOrdinal - OrdinalBase))));
+                        FunctionPtr = (IntPtr)((Int64)ModuleBase + FunctionRVA);
+                        break;
+                    }
+                }
+            }
+            catch
+            {
+                // Catch parser failure
+                throw new InvalidOperationException("Failed to parse module exports.");
+            }
+
+            if (FunctionPtr == IntPtr.Zero)
+            {
+                // Export not found
+                throw new MissingMethodException(FunctionHash + ", export hash not found.");
             }
             return FunctionPtr;
         }
